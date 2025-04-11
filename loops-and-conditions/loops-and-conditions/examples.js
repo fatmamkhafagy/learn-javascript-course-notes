ea55f136bee598f79eb5587bd73e5dcf1cec4ce4
fatmamkhafagy <shield.sniper.github@gmail.com>
2025-04-11 08:35:41 +0200

    Add examples.js with sample code for loops and conditions

---
+++ loops-and-conditions/loops-and-conditions/examples.js
@@ -0,0 +1,10 @@
+// Loop بسيط يطبع Hello 5 مرات
+for (let i = 0; i < 5; i++) {
+  console.log("Hello " + i);
+}
+
+// شرط بسيط باستخدام if
+let age = 22;
+if (age >= 18) {
+  console.log("أنت بالغ");
+}
