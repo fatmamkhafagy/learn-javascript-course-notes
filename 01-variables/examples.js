4db0f6dac7a9f1963f8d14966d245096b52b299a
fatmamkhafagy <shield.sniper.github@gmail.com>
2025-04-13 17:01:42 +0200

    Add example for console output using console.log()

--- 01-variables/examples.js
+++ 01-variables/examples.js
@@ -1,1 +1,14 @@
+// Chapter 1 - Lesson 01: Learn JavaScript (for Developers)
+// Topic: Console Output
+
+// This example demonstrates how to print a message to the browser console using console.log()
+// console.log() is a built-in JavaScript function used to output text for debugging and testing purposes.
+
+// It displays messages in the browser's developer console.
+// Very useful for tracking the flow of a program and identifying issues.
+
+// Expected Output:
+// Starting Textio server...
+
+console.log("Starting Textio server...");
 
