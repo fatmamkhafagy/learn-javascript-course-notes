793251a0cfbd90b95bd15f48732c9da3e6d594af
fatmamkhafagy <shield.sniper.github@gmail.com>
2025-04-14 17:56:34 +0200

    Create examples.js

---
+++ 01-variables/08-Increment and Decrement/examples.js
@@ -0,0 +1,20 @@
+#### 2. `examples.js`
+
+```js
+// Increment by 1
+let bootdevCourseRating = 4;
+bootdevCourseRating++;
+console.log(bootdevCourseRating); // 5
+
+// Increment by 5
+bootdevCourseRating += 5;
+console.log(bootdevCourseRating); // 10
+
+// Decrement by 1
+bootdevCourseRating = 11;
+bootdevCourseRating--;
+console.log(bootdevCourseRating); // 10
+
+// Decrement by 5
+bootdevCourseRating -= 5;
+console.log(bootdevCourseRating); // 5
