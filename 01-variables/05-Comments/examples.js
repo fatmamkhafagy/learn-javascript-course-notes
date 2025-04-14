db7260e84c7eaec8d297855050183716c1ca420b
fatmamkhafagy <shield.sniper.github@gmail.com>
2025-04-14 12:40:06 +0200

    Create examples.js

---
+++ 01-variables/05-Comments/examples.js
@@ -0,0 +1,31 @@
+// Single-line comment example
+// This variable stores the user's name
+let userName = "Sara";
+
+// This function says hello
+function greet(name) {
+  // Print a greeting to the console
+  console.log("Hello, " + name + "!");
+}
+
+greet(userName);
+
+/*
+  Multi-line comment example:
+  The following block of code calculates
+  the area of a rectangle
+*/
+
+let width = 5;
+let height = 10;
+let area = width * height;
+
+console.log("Area of the rectangle is:", area);
+
+/*
+  You can also use multi-line comments
+  to temporarily disable code:
+*/
+
+// let age = 25;
+// console.log("User age is:", age);
