a33c0ffa99cdf3194a237d26482d54154fb177cd
fatmamkhafagy <shield.sniper.github@gmail.com>
2025-04-14 12:30:31 +0200

    Create examples.js

---
+++ 01-variables/04: Why JavaScript?/examples.js
@@ -0,0 +1,22 @@
+// Example 1: Print a message to the browser console
+console.log("JavaScript is running in the browser!");
+
+// Example 2: Show a popup alert
+alert("Hello from JavaScript!");
+
+// Example 3: Handle a button click (this only works in an HTML file)
+function greetUser() {
+  alert("You clicked the button!");
+}
+
+/*
+  Notes for Beginners:
+
+  - console.log() → This prints a message in the browser console. Developers use it to test and debug.
+  - alert() → This shows a popup message to the user in the browser.
+  - function greetUser() → This is a function you can connect to a button in an HTML page.
+
+  If you want to use greetUser() in an HTML file, use it like this:
+
+  <button onclick="greetUser()">Click Me</button>
+*/
